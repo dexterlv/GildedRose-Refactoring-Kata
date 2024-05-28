925350e7019ef4f91a5d6b2102c10c5016b826b5
Dmitrijs Jakovlevs <dimmy.jey@gmail.com>
2024-05-29 02:06:56 +0300

    additional unit tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,106 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_regular(self):
+        items = [
+            Item("foo", 2, 2),
+            Item("bro", 0, 3),
+            Item("old", 0, 1)
+        ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+        self.assertEqual("bro", items[1].name)
+        self.assertEqual(-1, items[1].sell_in)
+        self.assertEqual(1, items[1].quality)
+
+        self.assertEqual("old", items[2].name)
+        self.assertEqual(-1, items[2].sell_in)
+        self.assertEqual(0, items[2].quality)
+
+    def test_aged_brie(self):
+        items = [
+            Item("Aged Brie", 2, 2),
+            Item("Aged Brie", 0, 3),
+            Item("Aged Brie", 0, 49)
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual("Aged Brie", items[0].name)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(3, items[0].quality)
+
+        self.assertEqual("Aged Brie", items[1].name)
+        self.assertEqual(-1, items[1].sell_in)
+        self.assertEqual(5, items[1].quality)
+
+        self.assertEqual("Aged Brie", items[2].name)
+        self.assertEqual(-1, items[2].sell_in)
+        self.assertEqual(50, items[2].quality)
+
+
+    def test_sulfuras(self):
+        items = [
+            Item("Sulfuras, Hand of Ragnaros", 2, 2)            
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(2, items[0].sell_in)
+        self.assertEqual(2, items[0].quality)
+
+    def test_backstage_passes(self):
+        items = [
+            Item("Backstage passes to a TAFKAL80ETC concert", 14, 5),
+            Item("Backstage passes to a TAFKAL80ETC concert", 7, 5),
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 5),
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 49)
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[0].name)
+        self.assertEqual(13, items[0].sell_in)
+        self.assertEqual(6, items[0].quality)
+
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[1].name)
+        self.assertEqual(6, items[1].sell_in)
+        self.assertEqual(7, items[1].quality)
+
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[2].name)
+        self.assertEqual(0, items[2].sell_in)
+        self.assertEqual(8, items[2].quality)
+
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[3].name)
+        self.assertEqual(0, items[3].sell_in)
+        self.assertEqual(50, items[3].quality)
+
+    def test_conjured(self):
+        items = [
+            Item("Conjured Mana Cake", 2, 3),
+            Item("Conjured Mana Cake", 0, 5),
+            Item("Conjured Mana Cake", 0, 2)
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual("Conjured Mana Cake", items[0].name)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+        self.assertEqual("Conjured Mana Cake", items[1].name)
+        self.assertEqual(-1, items[1].sell_in)
+        self.assertEqual(1, items[1].quality)
+
+        self.assertEqual("Conjured Mana Cake", items[2].name)
+        self.assertEqual(-1, items[2].sell_in)
+        self.assertEqual(0, items[2].quality)    
 
         
 if __name__ == '__main__':
